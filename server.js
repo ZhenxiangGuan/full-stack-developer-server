6a11a99c054dff6522ce999fac817f640a8d7986
Steven <steveng@Stevens-Macbook-Pro.local>
2022-04-06 14:32:39 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+import helloController
+  from "./controllers/hello-controller.js";
+import express from 'express';
+import cors from 'cors';
+import userController
+  from "./controllers/user-controller.js";
+import tuitController from "./controllers/tuits-controller.js";
+const app = express();
+app.use(cors());
+app.use(express.json());
+userController(app);
+tuitController(app);
+helloController(app);
+app.get('/', (req, res) => {
+  res.send('Welcome to Full Stack Development!')})
+app.listen(process.env.PORT || 4000);
+
+
+
+
+
+
+
+
+
